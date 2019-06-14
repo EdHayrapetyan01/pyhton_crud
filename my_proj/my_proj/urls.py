2f5f39fb2bd49c7d9d8ff53a329893ab22b9b019
Ed <hayrapetyan-edo@inbox.ru>
2019-06-14 15:02:06 +0400

    add restframework

--- my_proj/my_proj/urls.py
+++ my_proj/my_proj/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.conf.urls import url, include
+from rest_framework import routers
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('books.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
